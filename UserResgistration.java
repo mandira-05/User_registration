200835878a8cb7ebdb722be9ba40832300c5132f
mandira-05 <mandira.sn.gowda@gmail.com>
2022-11-13 10:58:04 +0530

    addded UC3 to check valid email

--- UserResgistration.java
+++ UserResgistration.java
@@ -42,10 +42,26 @@ public class UserResgistration {
        }else {
            System.err.println(lastName+ " is an invalid Last Name");
        }
-
-
    }
 
+   public static void checkValidEmail(String email) {
+       boolean isEmail;
+       String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . +_]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+       Pattern patternObject = Pattern.compile(emailRegex);
+
+       if (email == null) {
+           isEmail = false;
+       }
+
+       Matcher matcherObject = patternObject.matcher(email);
+       isEmail = matcherObject.matches();
+
+       if (isEmail) {
+           System.out.println(email+ " is a Valid Email address \n");
+       }else {
+           System.out.println(email+ " is a invalid Email address \n");
+       }
+   }
 
     public static void main(String[] args) {
 
@@ -59,7 +75,9 @@ public class UserResgistration {
         String lastName = scanner.next();
         checkValidLastName(lastName);
 
-
+        System.out.println("Enter your email address");
+        String email = scanner.next();
+        checkValidEmail(email);
 
         scanner.close();
 
