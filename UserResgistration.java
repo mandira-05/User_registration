450af998d950507a21a687faacde1908e25a690e
mandira-05 <mandira.sn.gowda@gmail.com>
2022-11-27 17:54:38 +0530

    Added UC5-password-rule 1

--- UserResgistration.java
+++ UserResgistration.java
@@ -63,6 +63,48 @@ public class UserResgistration {
        }
    }
 
+   public static void checkValidMobileNumber(String mobileNumber) {
+
+       boolean isMobileNumber;
+       //String mobNumRegex ="^[0-9 ]{3}[0-9]{10}$";
+       String mobNumRegex ="^[0-9]{2}' '{1}[0-9]{10}$";
+       Pattern patternObject = Pattern.compile(mobNumRegex);
+       //Pattern patternObject = Pattern.compile("^[0-9]{2}(\\s)?[0-9]{10}$");
+       if (mobileNumber == null) {
+           isMobileNumber = false;
+       }
+       Matcher matcherObject = patternObject.matcher(mobileNumber);
+       isMobileNumber = matcherObject.matches();
+
+       if (isMobileNumber) {
+           System.out.println(mobileNumber+ " is a valid mob number");
+       } else {
+           System.out.println(mobileNumber+ " is not a valid mob number");
+       }
+   }
+
+   public static void checkValidPassword(String password) {
+       boolean isPassword;
+       String passwordRegex = "^[ A-Za-z0-9_@./$#&+-]{8,}$";
+
+       Pattern patternObject = Pattern.compile(passwordRegex);
+
+       if (password == null ) {
+           isPassword = false;
+       }
+
+       Matcher matcherObject = patternObject.matcher(password);
+       isPassword = matcherObject.matches();
+
+       if (isPassword) {
+           System.out.println(password+ " is valid password ");
+       } else {
+           System.out.println(password+ " is invalid password ");
+       }
+
+
+   }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -70,16 +112,25 @@ public class UserResgistration {
         System.out.println("Enter your first name");
         String firstName = scanner.next();
         checkValidFirstName(firstName);
-
+//
         System.out.println("Enter your last name");
         String lastName = scanner.next();
         checkValidLastName(lastName);
-
+//
         System.out.println("Enter your email address");
         String email = scanner.next();
         checkValidEmail(email);
+//
+        //System.out.println("Enter your Mobile number");
+        //String mobileNumber = scanner.next();
+        //checkValidMobileNumber(mobileNumber);
+
+
+
+        System.out.println("Enter your password");
+        String password = scanner.next();
+        checkValidPassword(password);
 
         scanner.close();
-
     }
 }
