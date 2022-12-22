d45a41708159b3dc5de8d46a3d3bda77a5710065
mandira-05 <mandira.sn.gowda@gmail.com>
2022-12-22 07:52:17 +0530

    Added rule2- to have atleats special character

--- UserResgistration.java
+++ UserResgistration.java
@@ -87,7 +87,7 @@ public class UserResgistration {
     public static void checkValidPassword(String password) {
 
         boolean isPassword;
-        String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$";
+        String passwordRegex = "^^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$";
         Pattern patternObject = Pattern.compile(passwordRegex);
         if (password == null) {
             isPassword = false;
